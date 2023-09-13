0eb0d56e50628c07cfb19cd73eda537caf850ac9
huyvu85 <ngoisaodo@gmail.com>
2023-09-13 23:56:13 +0700

    Delete config.h

--- config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/* The way how "handedness" is decided (which half is which),
-see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
-for more options.
-*/
-
-#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
-#define CAPS_WORD_INVERT_ON_SHIFT
