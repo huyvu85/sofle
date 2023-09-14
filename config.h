8c88644b198b84e47e9c7687ca8e1d8c6a0f4f72
huyvu85 <ngoisaodo@gmail.com>
2023-09-14 19:08:30 +0700

    Add files via upload

---
+++ config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define SPLIT_WPM_ENABLE
+#define OLED_TIMEOUT 30000
+#define OLED_BRIGHTNESS 120
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define CAPS_WORD_INVERT_ON_SHIFT
+#define CAPS_WORD_IDLE_TIMEOUT 0
